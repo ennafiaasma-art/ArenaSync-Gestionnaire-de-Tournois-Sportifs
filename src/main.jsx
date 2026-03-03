cbb99f4f5e9c3d042d2fec4c6483afcf2d4c76c7
ennafiaasma-art <ennafiaasma@gmail.com>
2026-03-03 12:34:26 +0000

    la creactin des package  jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+
+ const root=createRoot(document.getElementById("root"))
+ root.render(
+
+<header class="bg-blue-400 d-flex flex"> 
+    <h1> bonjour</h1>
+    <img src="" alt="" />
+    <img src="" alt="" />
+</header>
+)
